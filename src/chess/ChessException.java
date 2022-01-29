a1b87ec3575842870d665b6372513a2a3f199c83
andersonlc2 <acorreia610@gmail.com>
2022-01-29 14:06:44 -0300

    Moving pieces

---
+++ src/chess/ChessException.java
@@ -0,0 +1,10 @@
+package chess;
+
+import bordergame.BoardException;
+
+public class ChessException extends BoardException {
+
+    public ChessException(String msg) {
+        super(msg);
+    }
+}
